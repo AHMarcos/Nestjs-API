30f3d1c1933a6d9467984fb0795a0f45f52f6797
AHMarcos <marcos.alarcon@vallegrande.edu.pe>
2020-08-15 22:17:04 -0500

    API en nestjs

---
+++ src/mensajes/mensajes.controller.ts
@@ -0,0 +1,46 @@
+import { Controller, Post, Body, Get, Put, Delete, Res, HttpStatus, Param } from '@nestjs/common';
+import { CreateMensajeDto } from './dto/create-mensaje-dto';
+import { MensajesService } from './mensajes.service';
+
+@Controller('mensajes')
+export class MensajesController {
+
+    constructor(private mensajesServices: MensajesService){
+    }
+
+    @Post()
+    create (@Body() createMensajeDto:CreateMensajeDto, @Res() response) {
+        this.mensajesServices.createMensaje(createMensajeDto).then( mensaje => { 
+            response.status(HttpStatus.CREATED).json(mensaje); 
+        }).catch( () => {
+           response.status(HttpStatus.FORBIDDEN).json({mensaje: 'error en la creacion del sms'});
+        }); 
+    }
+
+    @Get()
+    getAll(@Res() response) {
+        this.mensajesServices.getAll().then(mensajesList => {
+            response.status(HttpStatus.OK).json(mensajesList); 
+        }).catch( () => {
+            response.status(HttpStatus.FORBIDDEN).json({mensaje: 'error en la obtención de sms'});
+        });
+    }
+
+    @Put(':id')
+    update(@Body() updateMensajeDto: CreateMensajeDto, @Res() response, @Param('id') idMensaje) {
+        this.mensajesServices.updateMensaje(idMensaje, updateMensajeDto).then(mensaje => {
+            response.status(HttpStatus.OK).json({mensaje})
+        }).catch( () => {
+            response.status(HttpStatus.FORBIDDEN).json({mensaje:'error en la actualizacion del sms'});
+        });
+    }
+
+    @Delete(':id')
+    delete(@Res() response, @Param('id') idMensaje){
+        this.mensajesServices.deleteMensaje(idMensaje).then(res => {
+            response.status(HttpStatus.OK).json(res);
+        }).catch( () => {
+            response.status(HttpStatus.FORBIDDEN).json({mensaje:'error en la eliminacion del sms'});
+        })
+    }
+}
